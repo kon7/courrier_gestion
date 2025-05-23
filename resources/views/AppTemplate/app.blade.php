692d5358c9bbd7723ef53fab31a66d0210da0879
unknown <konkobojustealane@gmail.com>
2025-05-23 19:04:22 +0000

    debug31

--- resources/views/AppTemplate/app.blade.php
+++ resources/views/AppTemplate/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="Responsive HTML Admin Dashboard Template based on Bootstrap 5">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="author" content="NobleUI">
     <meta name="keywords" content="nobleui, bootstrap,admin, dashboard, template, responsive">
 

c55d9d4a9e0ce597bd5367407c89c87bed0311aa
unknown <konkobojustealane@gmail.com>
2025-05-23 15:34:55 +0000

    debug20

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ if [ ! -f .env ]; then
 fi
 
 # 👉 Définir dynamiquement l'APP_URL
-APP_URL=${APP_URL:-http://sgc-q0lb.onrender.com}
+APP_URL=${APP_URL:-sgc-q0lb.onrender.com}
 sed -i "s|^APP_URL=.*|APP_URL=${APP_URL}|" .env
 echo "🌍 APP_URL défini sur ${APP_URL}"
 
